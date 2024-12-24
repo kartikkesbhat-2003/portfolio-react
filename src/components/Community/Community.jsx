338057f72be0da47cc597b538c96ee430d42ef7d
Kartik Kesbhat <kartikkeshbhat2003@gmail.com>
2024-12-24 23:47:06 +0530

    normal changes

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -10,21 +10,21 @@ const communityData = [
     logo: pyf_logo,
     communityName: 'Prayas Youth Forum & Social Awareness Foundation, Pune',
     role: 'Social Media Head',
-    duration: '2015 - Present',
+    duration: '2022 - Present',
     description: 'A non-profit empowering underprivileged communities through impactful initiatives.'
   },
   {
     logo: gdsc_logo,
     communityName: 'Google Developer Student Clubs SCOE',
     role: 'Graphics Designer',
-    duration: '2019 - Present',
+    duration: '2023 - 2024',
     description: 'A platform fostering innovation and problem-solving using technology.'
   },
   {
     logo: ecell_logo,
     communityName: 'E-Cell SCOE',
     role: 'Member',
-    duration: '2018 - Present',
+    duration: '2023 - Present',
     description: 'An entrepreneurial cell promoting startup culture among students.'
   }
 ];
